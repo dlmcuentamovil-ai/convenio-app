41f308c9a6b92b71e8f265b91094c58b5d06e3e6
dlmcuentamovil-ai <dlmcuentamovil@gmail.com>
2026-06-03 13:03:47 +0200

    Update respuestas_basico.js

--- respuestas_basico.js
+++ respuestas_basico.js
@@ -39,8 +39,9 @@ window.RESPUESTAS_BASICO = {
   - Segundo bienio un 10%
   - Tercer bienio 15%` }, quinquenios:{ texto:`Se cobrarán a partir del tercer bienio incrementando un 7% por quinquenio` } },
   pagas: { imagenes: [], verano:{ texto:`Paga de Verano: Se devengará del 1 de enero al 30 de junio.
-  La paga extraordinaria de Verano se hará efectiva el día 16 del mes  de julio.` }, navidad:{ texto:`Paga de Navidad: Se devengará del 1 de julio al 31 de diciembre.
-  La paga de Navidad se hará efectiva el día 17 del mes de diciembre. ` } },
+  La paga extraordinaria de Verano se hará efectiva el día 16 del mes  de julio.` }, sanmartindeporres:{ texto:`Paga de San Martín de Porres: Se devengará desde el 1 de enero al 31 de diciembre  y se abonará con la nómina del mes de octubre y en proporción al tiempo trabajado. 
+  Esta paga se hara efectiva en el me Octubre y en proporción al tiempo trabajado` }, navidad:{ texto:`Paga de Navidad: Se devengará del 1 de julio al 31 de diciembre.
+  La paga de Navidad se hará efectiva el día 17 del mes de diciembre. ` }, },
   economia: { imagenes: [], salario:{ texto:"" }, complementos:{ texto:"" }, dietas:{ texto:"" }, kilometraje:{ texto:"" } },
   excedencias: { imagenes: [], voluntaria:{ texto:"" }, forzosa:{ texto:"" }, cuidado:{ texto:"" } },
   conductores: { imagenes: [], carnet:{ texto:"" }, funciones:{ texto:"" }, plus:{ texto:"" } },
